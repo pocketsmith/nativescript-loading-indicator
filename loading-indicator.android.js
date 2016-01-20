8818a5b19a0c8dc1f5d7e57694fe858b314e4938
Nathan Walker <walkerrunpdx@gmail.com>
2016-01-19 16:58:30 -0800

    allow custom message for android on show

--- loading-indicator.android.js
+++ loading-indicator.android.js
@@ -2,9 +2,9 @@ var application = require("application");
 
 var indicator = {};
 
-indicator.show = function() {
+indicator.show = function(msg) {
   if (context = this._getContext()) {
-    this._progressDialog = android.app.ProgressDialog.show(context, "", "Loading");
+    this._progressDialog = android.app.ProgressDialog.show(context, "", msg || "Loading");
     return this._progressDialog;
   }
 };
